1ed19300047649ffff9d0a90719e3c87f41a4f6c
Andre King <andreking1031@gmail.com>
2020-09-02 19:22:20 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -9,14 +9,14 @@ class Person
     self.partner = person
     if person.class != Person
       begin
-      raise PartnerError
+        raise PartnerError
       rescue PartnerError => error
-      puts error.message
-    end
+          puts error.message
+      end
     else
       person.partner = self
     end
-  end
+end
   
   class PartnerError < StandardError
     def message
